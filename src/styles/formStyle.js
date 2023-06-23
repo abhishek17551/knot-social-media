a17a1b8f47c14d474c80f1f08b9bd34c9c390dac
abhishek17551 <ababhishek113@gmail.com>
2023-06-24 00:30:57 +0530

    style:styles for form elements

---
+++ src/styles/formStyle.js
@@ -0,0 +1,18 @@
+const inputWrapper = {
+    display : 'flex',
+    justifyContent : 'flex-start',
+    alignItems : 'flex-start',
+    marginBottom : '5'
+}
+
+const inputRightWrapper = {
+    width : '70%',
+    marginBottom : '0'
+}
+
+const inputLeftWrapper = {
+    width: "30%",
+    marginBottom : '0'
+}
+
+export {inputWrapper,inputRightWrapper,inputLeftWrapper}
